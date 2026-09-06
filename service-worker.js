9de06c4a341aa70fc53b150b4efc26ce086049d6
miyukutopafe-ui <miyukutopafe@gmail.com>
2026-09-06 12:45:21 +0900

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE = "ta-investor-v1";
+const CACHE = "ta-investor-v04";
 const ASSETS = ["./","./index.html","./manifest.webmanifest","./icon-180.png","./icon-512.png"];
 
 self.addEventListener("install", event => {
