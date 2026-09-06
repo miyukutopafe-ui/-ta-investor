c702570980b5fd60af8c51fc64ca379eeae0b8dc
miyukutopafe-ui <miyukutopafe@gmail.com>
2026-09-06 11:51:37 +0900

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,22 @@
+const CACHE = "ta-investor-v1";
+const ASSETS = ["./","./index.html","./manifest.webmanifest","./icon-180.png","./icon-512.png"];
+
+self.addEventListener("install", event => {
+  event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(ASSETS)));
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k))))
+  );
+  self.clients.claim();
+});
+
+self.addEventListener("fetch", event => {
+  const url = new URL(event.request.url);
+  if (url.hostname === "api.twelvedata.com") return;
+  event.respondWith(
+    caches.match(event.request).then(hit => hit || fetch(event.request))
+  );
+});
